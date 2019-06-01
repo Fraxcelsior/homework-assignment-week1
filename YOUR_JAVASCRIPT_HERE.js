3d323d854c155c6b98a42916e77b4449a5c4ab55
Fraxcelsior <marten_bonnema@hotmail.com>
2019-06-01 13:08:14 +0200

    pickUpItems and rest functions defined and working, inn added to HTML.

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -10,8 +10,46 @@ let hero = {
     }
 };
 
-function rest() {};
 
-function pickUpItem() {};
+let inn = document.getElementById("inn");
+    console.log(inn);
+    inn.onclick = function () {
+        console.log("got clicked!");
+        rest(hero);
+    }
+
+
+
+function rest(hero) {
+    console.log(hero);
+    hero.health = 10;
+    console.log(hero.health);
+    console.log("Health set at 10");
+    return hero;
+};
+
+function pickUpItem(hero, weapon) {
+    hero.inventory.push(weapon);
+    console.log(hero.inventory);
+};
 
 function equipWeapon() {};
+
+
+
+
+
+/*workbench
+
+
+function myTest(hero) {
+hero.health = 5;
+console.log(hero.health);
+rest(hero);
+}
+
+myTest(hero);
+
+
+
+*/
